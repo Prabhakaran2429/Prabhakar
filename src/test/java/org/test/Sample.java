240eb5634329839abcf35d567d548fc99d376a50
prabhakaran2429 <prabhakarankece@gmail.com>
2022-12-21 12:18:03 +0530

    New

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("ghjfhweifuj");
 		System.out.println("4526764gyfuy1");
-		System.out.println("dghwkhw2");
+		System.out.println("hsidjgfiugds45677u");
 
 }
 }
